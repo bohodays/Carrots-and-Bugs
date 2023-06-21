9e11f4be41cae66d66dd82ae01e50dbbf07bba1d
bohodays <bohodays0316@gmail.com>
2023-06-21 22:22:03 +0900

    update : class화

--- src/field.js
+++ src/field.js
@@ -4,6 +4,11 @@ import * as sound from "./sound.js";
 
 const CARROT_SIZE = 80;
 
+export const ItemType = Object.freeze({
+  carrot: "carrot",
+  bug: "bug",
+});
+
 export default class Field {
   constructor(carrotCount, bugCount) {
     this.carrotCount = carrotCount;
@@ -55,9 +60,9 @@ export default class Field {
       // 당근
       target.remove();
       sound.playCarrot();
-      this.onItemClick && this.onItemClick("carrot");
+      this.onItemClick && this.onItemClick(ItemType.carrot);
     } else if (target.matches(".bug")) {
-      this.onItemClick && this.onItemClick("bug");
+      this.onItemClick && this.onItemClick(ItemType.bug);
     }
   };
 }
--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 "use strict";
 
-import Field from "./field.js";
+import Field, { ItemType } from "./field.js";
+import * as sound from "./sound.js";
 
 export const Reason = Object.freeze({
   win: "win",
@@ -86,13 +87,13 @@ class Game {
     if (!this.started) {
       return;
     }
-    if (item === "carrot") {
+    if (item === ItemType.carrot) {
       this.score++;
       this.updateScoreBoard();
       if (this.score === this.carrotCount) {
         this.stop(Reason.win);
       }
-    } else if (item === "bug") {
+    } else if (item === ItemType.bug) {
       // 벌레
       this.stop(Reason.lose);
     }
